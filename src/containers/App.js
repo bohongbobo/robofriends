627c954288a9b46f8ae3a136b8d1ee9ba9c477af
Bohong <bobo@Bohongs-MacBook.local>
2020-01-15 18:52:57 -0600

    update app.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -30,16 +30,20 @@ class App extends Component {
         const filteredRobot = robots.filter(robot => {
             return robot.name.toLowerCase().includes(searchfield.toLocaleLowerCase());
         })
-        return !robots.length ? <h1>Loading...</h1> : (
-            <div className='tc'>
+        if (!robots.length){
+            return <h1>Loading...</h1> 
+        } else {
+            return (
+                <div className='tc'>
                 <h1 className='f2'>RobotFriends</h1>
                 <SearchBox searchChange={this.onSearchChange} />
                 <Scroll>
                     <CardList robots={filteredRobot} />
                 </Scroll>
-                
-            </div>
-        ); 
+
+                </div>
+            )
+        }; 
     }
 }
 
