499edf4d2b2331cdf0bbd8ecef678874607a9fab
Bohong <bobo@Bohongs-MacBook.local>
2020-01-15 18:15:21 -0600

    update

---
+++ build/precache-manifest.720bcbd22341335cbb42495edfb03e8c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c719253318bd27544ed9ea453290b641",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e80c600ad78460bf37e2",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "0618f5ccb866c08d9292",
+    "url": "/static/css/main.5b433a7e.chunk.css"
+  },
+  {
+    "revision": "e80c600ad78460bf37e2",
+    "url": "/static/js/2.d6fca685.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.d6fca685.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0618f5ccb866c08d9292",
+    "url": "/static/js/main.47701c13.chunk.js"
+  },
+  {
+    "revision": "e3f6cca2499cc0ccc243",
+    "url": "/static/js/runtime-main.b2109bab.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
